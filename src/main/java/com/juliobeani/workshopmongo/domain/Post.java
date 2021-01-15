624a9c2a0374c3aa22da414b41875b0ae3305213
JulioMaiaB <julio_maia1@hotmail.com>
2021-01-15 17:14:04 -0300

    Projection of author's data with DTO

--- src/main/java/com/juliobeani/workshopmongo/domain/Post.java
+++ src/main/java/com/juliobeani/workshopmongo/domain/Post.java
@@ -1,23 +1,27 @@
 package com.juliobeani.workshopmongo.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.juliobeani.workshopmongo.dto.AuthorDTO;
+
 @Document
-public class Post {
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -57,11 +61,11 @@ public class Post {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
