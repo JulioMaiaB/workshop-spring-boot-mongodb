02deebfbeba138b6ce10f04af5dd0693943b6edb
JulioMaiaB <julio_maia1@hotmail.com>
2021-01-20 17:11:40 -0300

    Simple search with @Query

--- src/main/java/com/juliobeani/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/juliobeani/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.juliobeani.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.juliobeani.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.juliobeani.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	public List<Post> findByTitleContainingIgnoreCase(String text);
+	// $regex: ?0 significa que esta pegando o primeiro parametro do metodo
+	// $options: 'i' ignora letras maiusculas e minusculas
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
